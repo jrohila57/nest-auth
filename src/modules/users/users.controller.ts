d3ae980abc6cb2ecc90422e91b413e32dcbb2bfb
Jayant Rohila <jayant@bnimile.com>
2024-03-21 17:52:29 +0530

    ✨feature: Added Auth Guard

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -7,30 +7,35 @@ import {
   Delete,
   ParseIntPipe,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   async findAll() {
-    await this.usersService.findAll();
+    return await this.usersService.findAll();
   }
-
+  @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return await this.usersService.findOneById(id);
   }
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() createUsersDto: CreateUserDto) {
     return await this.usersService.create(createUsersDto);
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -39,6 +44,7 @@ export class UsersController {
     return await this.usersService.updateOneById(id, updateUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number) {
     return await this.usersService.deleteOneById(id);
