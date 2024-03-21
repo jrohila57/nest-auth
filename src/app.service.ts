e2665144e55e4251a6d13d51cd136aefa115139b
Jayant Rohila <jayant@bnimile.com>
2024-03-21 13:57:10 +0530

    🖥️ wip: Env configuration

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
+
+  getPort(): string {
+    return this.configService.get<string>('PORT')!;
+  }
   getHello(): string {
     return 'Hello World!';
   }
