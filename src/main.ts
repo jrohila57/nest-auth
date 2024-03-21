958dce9c0b3326875eac115c3e6433756868fa05
Jayant Rohila <jayant@bnimile.com>
2024-03-21 12:12:24 +0530

    ✨feature: Added Logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'fatal', 'error', 'warn', 'debug'],
+  });
+  try {
+    app.setGlobalPrefix('api/v1');
+    await app.listen(3000);
+    Logger.log(`Successfully started on port:${3000}`);
+  } catch (error) {
+    Logger.error(error);
+  }
 }
 bootstrap();
